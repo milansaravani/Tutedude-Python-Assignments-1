ecd7c3b3a85a2136477a95fd0e0853ddb3911960
Milan Saravani <62232608+milansaravani@users.noreply.github.com>
2026-01-15 12:18:50 +0530

    Add files via upload

---
+++ Task_1_perform_basic_mathematical_operations.py
@@ -0,0 +1,31 @@
+"""
+Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+	    Addition
+    	Subtraction
+	    Multiplication
+	    Division
+3.  Displays the results of each operation on the screen.
+
+"""
+
+num1 = float(input("Enter the First number : "))
+num2 = float(input("Enter the Second number : "))
+
+# Addition
+sum = int(num1 + num2)
+print("Addition : ",sum)
+
+# Subtraction
+sub = int(num1 - num2)
+print("Subtractio : ", sub)
+
+# Multiplication 
+mul = int(num1 * num2)
+print("Multiplication : ",mul)
+
+# Division
+div = num1 / num2
+print("Division : ",div)
---
+++ create_a_personalized_greeting.py
@@ -0,0 +1,12 @@
+"""
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+Hello, John Doe! Welcome to the python program.
+"""
+
+first_name = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
+
+print("Hello, " + first_name +" "+ last_name + "! Welcome to the python program")
